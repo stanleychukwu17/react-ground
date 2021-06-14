fdec48b78aaf4a0b1f33ea2f2f91334cf72e4bd8
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-14 09:03:48 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import './index.css';
 class Car extends React.Component {
     constructor (props) {
         super(props);
+        this.state = {'color':'blue'};
+    }
+
+    changeColor (color) {
+        console.log(color);
     }
 
     render () {
         return (
-            <div>This is my car</div>
+            <div>This is my car {this.state.color}</div>
+            <div><button onClick={this.changeColor}>change color</button></div>
         );
     }
 }
