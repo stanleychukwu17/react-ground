ebc60aac832fe57da2d6e04b68fa324c1f3eb4cd
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-14 08:31:41 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const myfirstelement = <h1>Hello React! From stanley</h1>
+const myfirstelement = <h1>Hello React! From</h1>
 
 ReactDOM.render(myfirstelement, document.getElementById('root'));
