1c4b8357c19824214e0e838cc72acb08fa24e418
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-16 10:36:26 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ let kal = [1,2,3], jon = [];
 let ind = kal.indexOf(2);
 let c1 = kal.splice(0, ind);
 let c3 = kal.splice(ind - 1);
-console.log(ind, c1, c3);
+let fin = [...c1, 4, ...c3];
+console.log(ind, c1, c3, fin);
 
 class Car extends React.Component {
     constructor (props) {
