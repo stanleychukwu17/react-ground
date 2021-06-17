4127c9647845e7844c2275fd9754746eec6f42a2
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-17 18:33:23 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -14,11 +14,6 @@ store_creator and subscriber
 i will try to give some of the examples below
 */
 
-let initStore = {
-    users : [],
-    'loading_data':true,
-    'err_msg':''
-};
 
 class Car extends React.Component {
     constructor (props) {
