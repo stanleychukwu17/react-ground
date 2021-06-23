0244bbbb44fea160aceb4318cca9606f422a7912
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-06-23 09:44:00 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 // lets work with testing of fetch api with the aysnc method
 let url = 'https://jsonplaceholder.typicode.com/users';
 function getU () {
-    console.log('getting');
+    fetch(url).then(rcv => console.log(rcv));
 }
 getU();
 
