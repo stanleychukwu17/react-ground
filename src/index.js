e113e13f874aa4f81b2ccafabe94ca52e1ca5bb2
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-01 11:12:17 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import './index.css';
 
 function Car (props) {
     let [color, setColor] = useState('blue');
+    let [man, setMan] = useState('john');
 
-    let changeColor = (wch) => {
-        setColor(wch);
-    }
+    let changeColor = (wch) => setColor(wch);
 
     return (
         <div>
             <div>This is my car {color}</div>
             <div><button onClick={changeColor.bind(this, (color === 'yellow') ? 'blue' : 'yellow')}>change color</button></div>
+            <div><button onClick={changeColor}>Test state</button></div>
         </div>
     );
 }
