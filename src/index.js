790b14de69b7c09b7c4e7b75dcf06bf9d148b8d6
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-01 11:06:35 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -3,35 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-
-
-class Car extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {'color':'blue'};
-    }
+function Car (props) {
+    this.state = {'color':'blue'};
 
     changeColor (color) {
         let nxt = (this.state.color === 'blue') ? 'red' : 'blue';
         this.setState({color: nxt});
     }
 
-    render () {
-        return (
-            <div>
-                <div>This is my car {this.state.color}</div>
-                <div><button onClick={this.changeColor.bind(this, 'yellow')}>change color</button></div>
-            </div>
-        );
-    }
-}
-
-function Jones (props) {
     return (
-        <div>my name is jones</div>
+        <div>
+            <div>This is my car {this.state.color}</div>
+            <div><button onClick={this.changeColor.bind(this, 'yellow')}>change color</button></div>
+        </div>
     );
 }
 
 
+
 ReactDOM.render(<Car color="orange" />, document.getElementById('root'));
-ReactDOM.render(<Jones  />, document.getElementById('boot'));
