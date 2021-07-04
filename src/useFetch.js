5a38575fd89b30a4db9eaef0c38f5417b7f3ecf6
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-07-04 09:08:28 +0100

    changes

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,7 +4,6 @@ export const useFetch = (url) => {
     let [data, setData] = useState(null);
     let reCount = useRef(0);
     reCount.current++;
-    console.log('we rendered')
 
     useEffect(() => {
         fetch(url).then(x =>x.json().then(x => { setData(x); }));
